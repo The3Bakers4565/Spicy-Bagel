42e3c48d12d04f86a6a9ebe84f4e51c3c444b6b6
The3Bakers4565 <85091887+The3Bakers4565@users.noreply.github.com>
2021-07-06 20:03:08 -0700

    Update Sleeves.lua

--- Functions/Arsenal/Arms/Sleeves.lua
+++ Functions/Arsenal/Arms/Sleeves.lua
@@ -1,9 +1,9 @@
 local a=game.ReplicatedStorage.Viewmodels.Arms.Vampire:Clone()
 a.Name="Sleeves"
 a.Parent=game.ReplicatedStorage.Viewmodels.Arms
-a["Right Arm"].RGlove:Destroy()
-a["Right Arm"].R.Welded.C1=CFrame.new(0,0,.775)
+a["Right Arm"].Flingerless_Right:Destroy()
+a["Right Arm"].Right_Sleeve.Welded.C1=CFrame.new(0,0,.775)
 a["Right Arm"].Transparency=0
-a["Left Arm"].LGlove:Destroy()
-a["Left Arm"].L.Welded.C1=CFrame.new(0,0,.775)
+a["Left Arm"].Fingerless_Left:Destroy()
+a["Left Arm"].Left_Sleeve.Welded.C1=CFrame.new(0,0,.775)
 a["Left Arm"].Transparency=0
