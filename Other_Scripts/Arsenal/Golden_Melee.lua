6fe8c2def322698591d72ba351171ec7bc2cae2c
The3Bakers4565 <85091887+The3Bakers4565@users.noreply.github.com>
2021-07-07 20:37:15 -0700

    Update Golden_Melee.lua

--- Other_Scripts/Arsenal/Golden_Melee.lua
+++ Other_Scripts/Arsenal/Golden_Melee.lua
@@ -1,2 +1,4 @@
+--Made By The3Bakers#4565
+--Discord: discord.gg/erdtnTSgng
 debug.setconstant(getsenv(game.Players.LocalPlayer.PlayerGui.GUI.Client).givetools,37,"Golden Knife")
 getsenv(game.Players.LocalPlayer.PlayerGui.GUI.Client).givetools()
