d3bf3fcb0e34ee899f0eed4a5bc23bd473ebe62a
The3Bakers4565 <85091887+The3Bakers4565@users.noreply.github.com>
2021-07-08 16:25:17 -0700

    Create Lag_Players.lua

---
+++ Other_Scripts/Arsenal/Lag_Players.lua
@@ -0,0 +1,18 @@
+if _G.lag then
+    return
+end
+_G.lag=true
+game.RunService.RenderStepped:Connect(function()
+    for _=1,5 do
+        game.ReplicatedStorage.Events.Trail:FireServer(
+            CFrame.new(math.random(),math.random(),math.random()),
+            Vector3.new(0,1,0),
+            {
+                
+            },
+            Color3.new(1,1,1),
+            "Railgun",
+            game.Players.LocalPlayer.Character.HumanoidRootPart
+        )
+    end
+end)
