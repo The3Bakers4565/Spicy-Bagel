1408877ab4c00505174ed6e86230dbd88521ed53
The3Bakers4565 <85091887+The3Bakers4565@users.noreply.github.com>
2021-07-10 13:38:57 -0700

    Update AutoFarm.lua

--- Other_Scripts/Phantom_Forces/AutoFarm.lua
+++ Other_Scripts/Phantom_Forces/AutoFarm.lua
@@ -1,12 +1,21 @@
+--Made By: The3Bakers#4565
+--discord.gg/erdtnTSgng
+--just incase people put this in their auto execute it wont double execute
+if _G.Loadded_AutoFarm then
+    return
+end _G.Loadded_AutoFarm=true
+--waits until game is loadded
 repeat wait()until game:IsLoaded()
 repeat wait()until pcall(function()print(game:GetService("Players").LocalPlayer.PlayerGui.Leaderboard.Main[game.Players.LocalPlayer.Team.Name].DataFrame.Data[game.Players.LocalPlayer.Name].Kills.Text)end)
 repeat wait()until not game.Players.LocalPlayer.PlayerGui:FindFirstChild("Loadscreen")
+--repeat spawn until player has spawned
 repeat
 if not game.Players.LocalPlayer.Character then
     repeat game:GetService("ControllerService").RemoteEvent:FireServer("spawn")wait(1)until game.Players.LocalPlayer.Character and game.Players.LocalPlayer.Character:IsDescendantOf(game.Workspace.Players)
 elseif not game.Players.LocalPlayer.Character:IsDescendantOf(game.Workspace.Players)then
     repeat game:GetService("ControllerService").RemoteEvent:FireServer("spawn")wait(1)until game.Players.LocalPlayer.Character and game.Players.LocalPlayer.Character:IsDescendantOf(game.Workspace.Players)
 end
+--get a enemy location without using getgc and getplayerhit bs
 local a=Vector3.new()
 for _,v in pairs(game.Workspace.Players:GetChildren())do
     if v.Name~=game.Players.LocalPlayer.Team.Name then
@@ -16,6 +25,7 @@ for _,v in pairs(game.Workspace.Players:GetChildren())do
         end
     end
 end
+--summons frag at position
 game:GetService("ControllerService").RemoteEvent:FireServer(
     "newgrenade",
     "FRAG",
@@ -65,13 +75,16 @@ game:GetService("ControllerService").RemoteEvent:FireServer(
         ["time"] = tick(),
         ["blowuptime"] = 0
     }
+        --stop fragging at 2 kills
 )wait(1)until tonumber(game:GetService("Players").LocalPlayer.PlayerGui.Leaderboard.Main[game.Players.LocalPlayer.Team.Name].DataFrame.Data[game.Players.LocalPlayer.Name].Kills.Text)>1
 game:GetService("Players").LocalPlayer.OnTeleport:Connect(function(x)
     if x==Enum.TeleportState.Started then
+            --execute script after teleported
         (syn and syn.queue_on_teleport or queue_on_teleport)("loadstring(game:HttpGet('https://raw.githubusercontent.com/The3Bakers4565/Spicy-Bagel/main/Other_Scripts/Phantom_Forces/AutoFarm.lua'))()")
     end
 end)
 local a={}
+--server hop
 for _,v in pairs(game.HttpService:JSONDecode(game:HttpGet("https://games.roblox.com/v1/games/"..game.PlaceId.."/servers/Public?limit=100")).data)do
     if v.playing<v.maxPlayers then
         table.insert(a,v.id)
