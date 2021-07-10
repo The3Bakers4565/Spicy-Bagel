e6b55571e84a5837ef29ce139cc91fa7b8ad6b1b
The3Bakers4565 <85091887+The3Bakers4565@users.noreply.github.com>
2021-07-10 12:56:35 -0700

    Update Auto_Farm.lua

--- Other_Scripts/Phantom_Forces/Auto_Farm.lua
+++ Other_Scripts/Phantom_Forces/Auto_Farm.lua
@@ -1,1 +1,81 @@
-return nil
+repeat wait()until game:IsLoaded()
+game:GetService("Players").LocalPlayer:WaitForChild("PlayerGui"):WaitForChild("Leaderboard"):WaitForChild("Main")[game.Players.LocalPlayer.Team.Name]:WaitForChild("DataFrame"):WaitForChild("Data")[game.Players.LocalPlayer.Name]:WaitForChild("Kills")
+repeat
+if not game.Players.LocalPlayer.Character then
+    repeat game:GetService("ControllerService").RemoteEvent:FireServer("spawn")wait(1)until game.Players.LocalPlayer.Character and game.Players.LocalPlayer.Character:IsDescendantOf(game.Workspace.Players)
+elseif not game.Players.LocalPlayer.Character:IsDescendantOf(game.Workspace.Players)then
+    repeat game:GetService("ControllerService").RemoteEvent:FireServer("spawn")wait(1)until game.Players.LocalPlayer.Character and game.Players.LocalPlayer.Character:IsDescendantOf(game.Workspace.Players)
+end
+local a=Vector3.new()
+for _,v in pairs(game.Workspace.Players:GetChildren())do
+    if v.Name~=game.Players.LocalPlayer.Team.Name then
+        if v:FindFirstChild("Player")then
+            a=v.Player.HumanoidRootPart.Position
+            break
+        end
+    end
+end
+game:GetService("ControllerService").RemoteEvent:FireServer(
+    "newgrenade",
+    "FRAG",
+    {
+        ["frames"] = {
+            {
+                ["v0"] = Vector3.new(math.random(),math.random(),math.random()),
+                ["glassbreaks"] = {},
+                ["t0"] = 0,
+                ["offset"] = Vector3.new(0, 0, 0),
+                ["rot0"] = CFrame.Angles(math.random(),math.random(),math.random()),
+                ["a"] = Vector3.new(0, -80, 0),
+                ["p0"] = game.Players.LocalPlayer.Character.HumanoidRootPart.Position+Vector3.new(math.random(),math.random(),math.random()),
+                ["rotv"] = Vector3.new(math.random(),math.random(),math.random())
+            },
+            {
+                ["v0"] = Vector3.new(math.random(),math.random(),math.random()),
+                ["glassbreaks"] = {},
+                ["t0"] = 0.018174676290405+math.random()/100,
+                ["a"] = Vector3.new(0, -80, 0),
+                ["offset"] = Vector3.new(math.random(),math.random(),math.random()),
+                ["rot0"] = CFrame.Angles(math.random(),math.random(),math.random()),
+                ["p0"] = a+Vector3.new(math.random(),math.random(),math.random()),
+                ["rotv"] = Vector3.new(math.random(),math.random(),math.random())
+            },
+            {
+                ["v0"] = Vector3.new(math.random(),math.random(),math.random()),
+                ["glassbreaks"] = {},
+                ["t0"] = 0.53664807322505+math.random()/100,
+                ["a"] = Vector3.new(0, -80, 0),
+                ["offset"] = Vector3.new(math.random(),math.random(),math.random()),
+                ["rot0"] = CFrame.Angles(math.random(),math.random(),math.random()),
+                ["p0"] = a+Vector3.new(math.random(),math.random(),math.random()),
+                ["rotv"] = Vector3.new(math.random(),math.random(),math.random())
+            },
+            {
+                ["v0"] = Vector3.new(math.random(),math.random(),math.random()),
+                ["glassbreaks"] = {},
+                ["t0"] = 0.66039720884605+math.random()/100,
+                ["a"] = Vector3.new(0, 0, 0),
+                ["offset"] = Vector3.new(math.random(),math.random(),math.random()),
+                ["rot0"] = CFrame.Angles(math.random(),math.random(),math.random()),
+                ["p0"] = a+Vector3.new(math.random(),math.random(),math.random()),
+                ["rotv"] = Vector3.new(math.random(),math.random(),math.random())
+            }
+        },
+        ["time"] = tick(),
+        ["blowuptime"] = 0
+    }
+)wait(1)until tonumber(game:GetService("Players").LocalPlayer.PlayerGui.Leaderboard.Main[game.Players.LocalPlayer.Team.Name].DataFrame.Data[game.Players.LocalPlayer.Name].Kills.Text)>1
+local queueont=(syn and syn.queue_on_teleport or queue_on_teleport)
+game:GetService("Players").LocalPlayer.OnTeleport:Connect(function(x)
+    if x==Enum.TeleportState.Started then
+        queueont("loadstring(game:HttpGet('https://raw.githubusercontent.com/The3Bakers4565/Spicy-Bagel/main/Other_Scripts/Phantom_Forces/Auto_Farm.lua'))()")
+    end
+end)
+for _,v in pairs(game.HttpService:JSONDecode(game:HttpGet("https://games.roblox.com/v1/games/"..game.PlaceId.."/servers/Public?limit=100")).data)do
+    if v.playing<v.maxPlayers then
+        table.insert(a,v.id)
+    end
+end
+while wait(1)do
+    game.TeleportService:TeleportToPlaceInstance(game.PlaceId,a[math.random(1,#a)])
+end
