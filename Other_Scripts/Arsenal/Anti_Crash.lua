93ebcaf0d1db0568f918bea2f0dd400c13c4623d
The3Bakers4565 <85091887+The3Bakers4565@users.noreply.github.com>
2021-07-24 13:43:34 -0700

    Update Anti_Crash.lua

--- Other_Scripts/Arsenal/Anti_Crash.lua
+++ Other_Scripts/Arsenal/Anti_Crash.lua
@@ -1,3 +1,5 @@
+--Made By: The3Bakers#4565
+--discord.gg/erdtnTSgng
 local old=require(game.ReplicatedStorage.Modules.ClientFunctions).CreateProjectile
 require(game.ReplicatedStorage.Modules.ClientFunctions).CreateProjectile=function(a,...)
     if tostring(a)~=game.Players.LocalPlayer.Name then
