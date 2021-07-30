87ebf5cf749702dcc79e8fd41cc09cdfe51304b1
The3Bakers4565 <85091887+The3Bakers4565@users.noreply.github.com>
2021-07-29 19:45:08 -0700

    Update Crash_All.lua

--- Other_Scripts/Arsenal/Crash_All.lua
+++ Other_Scripts/Arsenal/Crash_All.lua
@@ -1,5 +1,7 @@
+return game.Players.LocalPlayer:Kick("Patched Do Not Use")--[[
 --Made By: The3Bakers#4565
 --discord.gg/erdtnTSgng
 for _=1,5000 do
     game:GetService("ReplicatedStorage").Events.ReplicateProjectile:FireServer({"Baseball",0,Vector3.new(0,100,0),CFrame.new(),45,0,0,0,"Cone Launcher",Vector3.new(),false,{},1})
 end
+]]
