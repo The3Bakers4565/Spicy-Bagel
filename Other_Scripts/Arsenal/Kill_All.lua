f5944bb83264fb24ef35e6d59f8a6d5bf9bf25f5
The3Bakers4565 <85091887+The3Bakers4565@users.noreply.github.com>
2021-08-05 13:26:46 -0700

    Update Kill_All.lua

--- Other_Scripts/Arsenal/Kill_All.lua
+++ Other_Scripts/Arsenal/Kill_All.lua
@@ -16,25 +16,24 @@ end
 game.RunService.RenderStepped:Connect(function()
     for _,v in pairs(getenemys())do
         local Partpos=v.Character.Head.Position+Vector3.new(math.random(),math.random(),math.random())--get head pos with random position to prevent ss detection(old kill all had it so i added it)
-        local Packedstring=string.pack(--arsenals shitty anti remote spy shit that can be easly replicated
-            debug.getconstant(require(game:GetService("ReplicatedStorage").Modules.ClientFunctions).CreateProjectile,105),--the pack code the create projectile constant had it so i added it just incase it changed if the constant changed the old code was fffsi1fi1i1i1i2i1fi1i1i1
-            Partpos.X,--the x position of the part due to pack not being able to handle vector3? idk much about packs
-            Partpos.Y,--the y pos
-            Partpos.Z,--the z pos
-            "Autobow",--gun name (autobow is a projectile so it dosent require you to have it equipped)
-            --dont change most of the stuff down below unless you know what your doing
-            1,--headshot=1 none=0
-            0,--unknown
-            0,--backstab=1 none=0
-            0,--forcecrit=1 none=0
-            0,--morecrit=1 none=0
-            0,--spoof=2 none=0
-            0,--unkown
-            0,--Player Distance
-            1,--Current Spell
-            0,--scope=1 noscope=0
-            1--aircrit=1 none=0
-        )
+        local Packedstring=string.pack(
+                        debug.getconstant(require(game:GetService("ReplicatedStorage").Modules.ClientFunctions).CreateProjectile,105),
+                        Partpos.X,
+                        Partpos.Y,
+                        Partpos.Z,
+                        tostring(game.Players.LocalPlayer.PlayerGui.GUI.Client.Variables.gun.Value),
+                        0,--headshot=1 none=0
+                        0,--unknown
+                        0,--backstab=1 none=0
+                        0,--forcecrit=1 none=0
+                        0,--morecrit=1 none=0
+                        0,--spoof=2 none=0
+                        0,--unkown
+                        0,--Player Distance
+                        1,--Current Spell
+                        0,--scope=1 noscope=0
+                        0--aircrit=1 none=0
+                    )
         game.ReplicatedStorage.Events.HitPart:FireServer(v.Character.Head,Packedstring)
     end
 end)
