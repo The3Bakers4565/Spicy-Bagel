5d52c912a30fbe321181d7181f850b7c3dc677a8
The3Bakers4565 <85091887+The3Bakers4565@users.noreply.github.com>
2021-08-06 16:19:27 -0700

    Update Auto_Farm.lua

--- Other_Scripts/Arsenal/Auto_Farm.lua
+++ Other_Scripts/Arsenal/Auto_Farm.lua
@@ -1,2 +1,4 @@
 repeat wait()until game:IsLoaded()
-return game.Players.LocalPlayer:Kick("Connected to Autofarm.lua")
+repeat wait()until pcall(function()
+game.Players.LocalPlayer:Kick("Connected to Autofarm.lua")
+end)
