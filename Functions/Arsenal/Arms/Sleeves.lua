4397eea3da56d51a18993d3cdee8315e256f0994
The3Bakers4565 <85091887+The3Bakers4565@users.noreply.github.com>
2021-08-22 18:57:36 -0700

    Delete Functions directory

--- Functions/Arsenal/Arms/Sleeves.lua
+++
@@ -1,9 +0,0 @@
-local a=game.ReplicatedStorage.Viewmodels.Arms.Vampire:Clone()
-a.Name="Sleeves"
-a.Parent=game.ReplicatedStorage.Viewmodels.Arms
-a["Right Arm"].Flingerless_Right:Destroy()
-a["Right Arm"].Right_Sleeve.Welded.C1=CFrame.new(0,0,.775)
-a["Right Arm"].Transparency=0
-a["Left Arm"].Fingerless_Left:Destroy()
-a["Left Arm"].Left_Sleeve.Welded.C1=CFrame.new(0,0,.775)
-a["Left Arm"].Transparency=0
