1fbc48c6b0a403743caf6dd77bce6a7bcf9c2ed0
The3Bakers4565 <85091887+The3Bakers4565@users.noreply.github.com>
2022-01-15 11:15:36 -0800

    Create Invisiblity_Anti_Aim.lua

---
+++ Other/Bad_Business/Invisiblity_Anti_Aim.lua
@@ -0,0 +1,70 @@
+--Made by The3Bakers#4565
+--plz join https://discord.gg/yBEzKHB3Qr
+local PlayerTable=getupvalue(require(game.ReplicatedStorage.TS).Characters.GetCharacter,1)
+local IsAlive=function()
+    if PlayerTable[game.Players.LocalPlayer]then
+        if PlayerTable[game.Players.LocalPlayer].Parent==game.Workspace.Characters then
+            if PlayerTable[game.Players.LocalPlayer]:FindFirstChild("Root")then
+                return true
+            end
+        end
+    end
+    return false
+end
+local onspawn
+local oldroot
+local newroot
+local pos=Vector3.new()
+game.RunService.RenderStepped:Connect(function()
+    if IsAlive()then
+        for _,v in pairs(getupvalue(require(game.ReplicatedStorage.TS).Characters.GetCharacter,1)[game.Players.LocalPlayer].Body:GetChildren())do
+            if v.LocalTransparencyModifier~=0 then
+                v.LocalTransparencyModifier=0
+            end
+        end
+        if not onspawn then
+            onspawn=true
+            oldroot=PlayerTable[game.Players.LocalPlayer].Root
+            oldroot.CanCollide=false
+            pos=oldroot.Position
+            newroot=PlayerTable[game.Players.LocalPlayer].Root:Clone()
+            newroot.CanCollide=true
+            newroot.Name="NewRoot"
+            newroot.Parent=PlayerTable[game.Players.LocalPlayer]
+        end
+    else
+        if onspawn then
+            onspawn=false
+            oldroot=nil
+            newroot=nil
+        end
+    end
+end)
+local index
+local newindex
+index=hookmetamethod(game,"__index",function(a,b)
+    if oldroot and newroot then
+        if a==oldroot then
+            a=newroot
+        end
+    end
+    return index(a,b)
+end)
+newindex=hookmetamethod(game,"__newindex",function(a,b,c)
+    if oldroot and newroot then
+        if a==oldroot then
+            if tostring(b)=="CFrame"then
+                newroot.CFrame=c
+                c=c*CFrame.Angles(math.rad(-90),0,0)+
+                Vector3.new(0,-7.5,0)
+            elseif tostring(b)=="Velocity"then
+                newroot.Velocity=c
+                c=Vector3.new()
+            else
+                a=newroot
+            end
+        end
+    end
+    return newindex(a,b,c)
+end)
+game:GetService("StarterGui"):SetCore("SendNotification",{Title="Made By: The3Bakers#4565",Text="https://discord.gg/yBEzKHB3Qr",Duration=99999})
