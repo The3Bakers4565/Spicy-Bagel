d876bed1d6bb0ea943201d76bd29130940b34602
The3Bakers4565 <85091887+The3Bakers4565@users.noreply.github.com>
2022-01-16 18:30:04 -0800

    Create Frag_TP.lua

---
+++ Other/Bad_Business/Frag_TP.lua
@@ -0,0 +1,49 @@
+--Made by The3Bakers#4565
+--https://discord.gg/yBEzKHB3Qr
+local ClientTeam=""
+local PlayerTable=getupvalue(require(game.ReplicatedStorage.TS).Characters.GetCharacter,1)
+local GetEnemys=function()
+    local a={}
+    for _,v in pairs(game.Teams:GetChildren())do
+        if v.Name~=ClientTeam then
+            for _,c in pairs(v.Players:GetChildren())do
+                if PlayerTable[c.Value]then
+                    if PlayerTable[c.Value].Parent==game.Workspace.Characters then
+                        if PlayerTable[c.Value]:FindFirstChild("Root")then
+                            if not PlayerTable[c.Value].Root.ShieldEmitter.Enabled then
+                                a[c.Value]=PlayerTable[c.Value]
+                            end
+                        end
+                    end
+                end
+            end
+        end
+    end
+    return a
+end
+local target
+game.RunService.RenderStepped:Connect(function()
+    target=nil
+    for _,v in pairs(game.Teams:GetChildren())do
+        for _,c in pairs(v.Players:GetChildren())do
+            if c.Name==game.Players.LocalPlayer.Name then
+                ClientTeam=v.Name
+                break
+            end
+        end
+    end
+    for _,v in pairs(GetEnemys())do
+        target=v.Root
+        break
+    end
+end)
+game.Workspace.Throwables.ChildAdded:Connect(function(x)
+    local PrimaryBody=x:WaitForChild("Body"):WaitForChild("BodyPrimary")
+    if target then 
+        repeat  
+            PrimaryBody.CFrame=target.CFrame
+            game.RunService.RenderStepped:Wait()
+        until x==nil or not x:IsDescendantOf(game.Workspace)
+    end
+end)
+game:GetService("StarterGui"):SetCore("SendNotification",{Title="Made By: The3Bakers#4565",Text="https://discord.gg/yBEzKHB3Qr",Duration=99999})
