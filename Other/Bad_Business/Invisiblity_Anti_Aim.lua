aa9e50f039a4a1396800577dfcff8f575afb971a
The3Bakers4565 <85091887+The3Bakers4565@users.noreply.github.com>
2022-01-17 11:36:16 -0800

    Update Invisiblity_Anti_Aim.lua

--- Other/Bad_Business/Invisiblity_Anti_Aim.lua
+++ Other/Bad_Business/Invisiblity_Anti_Aim.lua
@@ -1,6 +1,6 @@
 --Made by The3Bakers#4565
 --https://discord.gg/yBEzKHB3Qr
-_G.Invisible_Baker=true--stuf to prevent a big error when my invisiblity anti aim is loadded
+_G.Invisible_Baker=true--stuf to prevent a big error when my knife aura is loadded
 if _G.KnifeAura_Baker then game:Shutdown()end
 local PlayerTable=getupvalue(require(game.ReplicatedStorage.TS).Characters.GetCharacter,1)--lag prevention
 local IsAlive=function()--simple function to detect if the player is alive
@@ -18,17 +18,17 @@ local oldroot--fix for later
 local newroot--fix for later
 game.RunService.RenderStepped:Connect(function()
     if IsAlive()then--check if player is alive
-        if not onspawn then--yes
-            onspawn=true
-            oldroot=PlayerTable[game.Players.LocalPlayer].Root
-            oldroot.CanCollide=false
-            newroot=PlayerTable[game.Players.LocalPlayer].Root:Clone()
-            newroot.CanCollide=true
-            newroot.Name="NewRoot"
-            newroot.Parent=PlayerTable[game.Players.LocalPlayer]
+        if not onspawn then--on spawn do
+            onspawn=true--fix
+            oldroot=PlayerTable[game.Players.LocalPlayer].Root--saves old root for later
+            oldroot.CanCollide=false--removes collisons for old root
+            newroot=PlayerTable[game.Players.LocalPlayer].Root:Clone()--clones root
+            newroot.CanCollide=true--adds collisions to new root
+            newroot.Name="NewRoot"--epic name for new root
+            newroot.Parent=PlayerTable[game.Players.LocalPlayer]--parent fix
         end
     else
-        if onspawn then
+        if onspawn then--shutup
             onspawn=false
             oldroot=nil
             newroot=nil
@@ -40,7 +40,7 @@ local newindex
 index=hookmetamethod(game,"__index",function(a,b)
     if oldroot and newroot then
         if a==oldroot then
-            a=newroot
+            a=newroot--index fixing to actually make the new root the new root
         end
     end
     return index(a,b)
@@ -49,17 +49,16 @@ newindex=hookmetamethod(game,"__newindex",function(a,b,c)
     if oldroot and newroot then
         if a==oldroot then
             if tostring(b)=="CFrame"then
-                newroot.CFrame=c
-                c=c*CFrame.Angles(math.rad(-90),0,0)+
-                Vector3.new(0,-7.5,0)--to go under ground O_O
+                newroot.CFrame=c--move the new root to were the old root would have been
+                c=c*CFrame.Angles(math.rad(-90),0,0)+Vector3.new(0,-7.5,0)--move the old root under the new root/under ground
             elseif tostring(b)=="Velocity"then
-                newroot.Velocity=c
-                c=Vector3.new()
+                newroot.Velocity=c--set the new roots velocity to what the old root would have been
+                c=Vector3.new()--set the old roots velocity to 0 because it would just build up velocity downwards and delete ur old root
             else
-                a=newroot
+                a=newroot--set other stuff to change new root instead of old root
             end
         end
     end
     return newindex(a,b,c)
 end)
-game:GetService("StarterGui"):SetCore("SendNotification",{Title="Made By: The3Bakers#4565",Text="https://discord.gg/yBEzKHB3Qr",Duration=99999})
+game:GetService("StarterGui"):SetCore("SendNotification",{Title="Made By: The3Bakers#4565",Text="https://discord.gg/yBEzKHB3Qr",Duration=99999})--plz join
