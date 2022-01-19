e1b6225317bfb135106ab3c7e5e0db276f266b41
The3Bakers4565 <85091887+The3Bakers4565@users.noreply.github.com>
2022-01-19 10:52:30 -0800

    Update Server_Breaker.lua

--- Other/Bad_Business/Server_Breaker.lua
+++ Other/Bad_Business/Server_Breaker.lua
@@ -8,4 +8,5 @@ game:GetService("RunService").RenderStepped:Connect(function()
     game:GetService("Players").LocalPlayer.PlayerGui.RoundCompleteGui.Enabled=false--remove annoying end round ui (breaks when this is executed)
 end)
 game:GetService("Players").LocalPlayer.Leaderboard:Destroy()--the main part of breaking the map votes
+game:GetService("StarterGui"):SetCore("SendNotification",{Title="Instructions",Text="Wait Until The Round is Over Then it Should Infinitly Loop Map Selections,The Script Only Works if You Are in Game And Do Not Leave.",Duration=99999})--to help people understand how it works abit more
 game:GetService("StarterGui"):SetCore("SendNotification",{Title="Made By: The3Bakers#4565",Text="https://discord.gg/yBEzKHB3Qr",Duration=99999})--plz join
