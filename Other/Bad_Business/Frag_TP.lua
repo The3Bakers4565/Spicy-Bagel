6b4a8142dd61d8903cafdb3e54157790fb1682f7
The3Bakers4565 <85091887+The3Bakers4565@users.noreply.github.com>
2022-01-22 22:34:19 -0800

    Update Frag_TP.lua

--- Other/Bad_Business/Frag_TP.lua
+++ Other/Bad_Business/Frag_TP.lua
@@ -1,14 +1,14 @@
 --Made by The3Bakers#4565
 --https://discord.gg/yBEzKHB3Qr
 local ClientTeam=""--will be updated later
-local PlayerTable=getupvalue(require(game.ReplicatedStorage.TS).Characters.GetCharacter,1)--so game dosent lag alot
+local PlayerTable=getupvalue(require(game:GetService("ReplicatedStorage").TS).Characters.GetCharacter,1)--so game dosent lag alot
 local GetEnemys=function()--simple get damagable enemys
     local a={}
     for _,v in pairs(game.Teams:GetChildren())do
         if v.Name~=ClientTeam then
             for _,c in pairs(v.Players:GetChildren())do
                 if PlayerTable[c.Value]then
-                    if PlayerTable[c.Value].Parent==game.Workspace.Characters then
+                    if PlayerTable[c.Value].Parent==game:GetService("Workspace").Characters then
                         if PlayerTable[c.Value]:FindFirstChild("Root")then
                             if not PlayerTable[c.Value].Root.ShieldEmitter.Enabled then
                                 a[c.Value]=PlayerTable[c.Value]
@@ -22,11 +22,11 @@ local GetEnemys=function()--simple get damagable enemys
     return a
 end
 local target--for later ;)
-game.RunService.RenderStepped:Connect(function()
+game:GetService("RunService").RenderStepped:Connect(function()
     target=nil--sets the target to nothing so it dosent glitch when the target is dead
     for _,v in pairs(game.Teams:GetChildren())do
         for _,c in pairs(v.Players:GetChildren())do
-            if c.Name==game.Players.LocalPlayer.Name then
+            if c.Name==game:GetService("Players").LocalPlayer.Name then
                 ClientTeam=v.Name--updates clients team
                 break
             end
@@ -44,24 +44,24 @@ game.RunService.RenderStepped:Connect(function()
         end
     end
 end)
-game.Workspace.Throwables.ChildAdded:Connect(function(x)
+game:GetService("Workspace").Throwables.ChildAdded:Connect(function(x)
     local PrimaryBody=x:WaitForChild("Body"):WaitForChild("BodyPrimary")--shit i pasted from older frag tps
     if target then 
         repeat  
             PrimaryBody.CFrame=target.CFrame--move nade to target
-            game.RunService.RenderStepped:Wait()--renderstepped:wait > wait
-        until x==nil or not x:IsDescendantOf(game.Workspace)--stop moving nade when deleted
+            game:GetService("RunService").RenderStepped:Wait()--renderstepped:wait > wait
+        until x==nil or not x:IsDescendantOf(game:GetService("Workspace"))--stop moving nade when deleted
     end
 end)
-game.Workspace.ChildAdded:Connect(function(x)--fix for new rounds
+game:GetService("Workspace").ChildAdded:Connect(function(x)--fix for new rounds
     if x.Name=="Throwables"then
         x.ChildAdded:Connect(function(x)
             local PrimaryBody=x:WaitForChild("Body"):WaitForChild("BodyPrimary")
             if target then 
                 repeat  
                     PrimaryBody.CFrame=target.CFrame
-                    game.RunService.RenderStepped:Wait()
-                until x==nil or not x:IsDescendantOf(game.Workspace)
+                    game:GetService("RunService").RenderStepped:Wait()
+                until x==nil or not x:IsDescendantOf(game:GetService("Workspace"))
             end
         end)
     end
